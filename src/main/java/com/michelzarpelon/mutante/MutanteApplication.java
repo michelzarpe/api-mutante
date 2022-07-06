6396bd719421ba5c32c4cd9278d6612db465dc3b
michel zarpelon <michelzarpe@gmail.com>
2022-07-06 18:08:03 -0300

    first

---
+++ src/main/java/com/michelzarpelon/mutante/MutanteApplication.java
@@ -0,0 +1,13 @@
+package com.michelzarpelon.mutante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutanteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MutanteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/michelzarpelon/mutante/MutanteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michelzarpelon.mutante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MutanteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
