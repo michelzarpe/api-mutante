8c6ba3ab889b7295319a8f1300be60cc2bbeaab5
michel zarpelon <michelzarpe@gmail.com>
2022-07-07 15:06:58 -0300

    configurando repository

--- src/main/java/com/michelzarpelon/mutante/model/ILineageRepository.java
+++ src/main/java/com/michelzarpelon/mutante/model/ILineageRepository.java
@@ -2,7 +2,12 @@ package com.michelzarpelon.mutante.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface ILineageRepository extends JpaRepository<Lineage,Long> {
+
+    @Transactional(readOnly = true)
+    public Lineage findLineageByDna(String dna);
+
 }
